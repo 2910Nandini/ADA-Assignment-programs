2d54d185db7c654b64b86c968bed92d410b53974
2910Nandini <100186370+2910Nandini@users.noreply.github.com>
2023-05-14 11:16:04 -0700

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,89 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+void swap(int* a, int* b)                          //assignment 17
+{
+
+    int t = *a;
+
+    *a = *b;
+
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+
+    int pivot = arr[high];
+
+    int i = (low - 1);
+
+    for (int j = low; j <= high- 1; j++)
+
+    {
+
+        if (arr[j] <= pivot)            //checking condition with pivot elem
+
+        {
+
+            i++;
+
+            swap(&arr[i], &arr[j]);
+
+        }
+
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)                 //low=starting index
+{                                                            //high=last index
+
+    if (low < high)
+
+    {
+
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);           //sorting separately
+
+        quickSort(arr, pi + 1, high);
+
+    }
+}
+
+//to print an array
+
+void printArray(int arr[], int size)
+{
+
+    int i;
+
+    for (i=0; i < size; i++)
+
+        cout<<" "<<arr[i];
+
+    cout<<"\n";
+}
+
+int main()
+{
+
+    int arr[50],i,n;
+    cout<<"Enter the size of array = \n";
+    cin>>n;
+    cout<<"Enter the array elements = \n";
+    for(i=0;i<n;i++)
+    {
+    cin>>arr[i];
+    }
+    quickSort(arr,0,n-1);
+    cout<<"Sorted array: \n";
+
+    printArray(arr, n);
+
+    return 0;
+}
